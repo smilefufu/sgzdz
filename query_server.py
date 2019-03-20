2536c281664cbf08ce73dbbb849acbd9873313d7
fufu <fufu.bluesand@gmail.com>
2019-03-20 20:17:20 +0800

    add new function

--- query_server.py
+++ query_server.py
@@ -2,8 +2,8 @@
 
 import os
 import sys
-reload(sys)
-sys.setdefaultencoding("utf-8")
+#reload(sys)
+#sys.setdefaultencoding("utf-8")
 import tornado.ioloop
 import tornado.web
 
@@ -16,9 +16,20 @@ class MainHandler(tornado.web.RequestHandler):
             cmd = 'echo "select email, level, last_login, cards from pigs_20 where level>=30 and cards like \'%%{}%%\';" | sqlite3 data.db | grep -v alita'.format(name)
         self.write("<pre>" + os.popen(cmd).read() + "</pre>")
 
+
+class MyHandler(tornado.web.RequestHandler):
+    def get(self):
+        name = self.get_argument("card")
+        if name == "all":
+            cmd = 'echo "select cards from pigs_20 level>=30;" | sqlite3 data.db | tr "," "\n" | grep -v 小乔 | egrep -v "^张辽" | egrep -v "^$" | sort | uniq -c | sort -k1n'
+        else:
+            cmd = 'echo "select email, level, last_login, cards from pigs_20 where level>=30 and cards like \'%%{}%%\';" | sqlite3 data.db'.format(name)
+        self.write("<pre>" + os.popen(cmd).read() + "</pre>")
+
 def make_app():
     return tornado.web.Application([
         (r"/", MainHandler),
+        (r"/a", MyHandler),
     ])
 
 if __name__ == "__main__":
