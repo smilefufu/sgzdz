5c42d1ff83228064d4a8295d32d413ef099270f4
fufu <fufu.bluesand@gmail.com>
2019-06-03 12:55:45 +0800

    update

--- robotctl.py
+++ robotctl.py
@@ -49,8 +49,8 @@ if __name__ == "__main__":
                 rows = c.fetchall()
                 now = datetime.datetime.now()
                 if not rows and now.hour in (0, 1, 2, 3, 4, 5, 6, 10, 12, 13, 15, 16, 18, 19, 21, 22):
-                    sql = "SELECT email FROM {} WHERE email not like 'adorable%' ORDER BY last_login DESC, level ASC LIMIT {}".format(table_name, MAX_ONLINE_COUNT - cnt)
-                    sql = "SELECT email FROM {} ORDER BY last_login DESC, level ASC LIMIT {}".format(table_name, MAX_ONLINE_COUNT - cnt)
+                    sql = "SELECT email FROM {} WHERE email not like 'adorable%' ORDER BY last_login ASC, level ASC LIMIT {}".format(table_name, MAX_ONLINE_COUNT - cnt)
+                    sql = "SELECT email FROM {} WHERE datetime(last_login) < datetime('now', '-10 minute', 'localtime') ORDER BY last_login ASC, level ASC LIMIT {}".format(table_name, MAX_ONLINE_COUNT - cnt)
                     log(sql)
                     c.execute(sql)
                     rows = c.fetchall()
